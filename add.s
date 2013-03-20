8ee4dce64f6f2c876b9e5b50e8987b3489a68490
Joby Poriyth <pcjoby@gmail.com>
2013-03-20 08:37:32 +0000

    An attempt to store a word in memory

--- add.s
+++ add.s
@@ -4,7 +4,11 @@ start:
 	mov	r0,	#5
 	mov	r1,	#4
 	add	r2,	r0,	r1
-
+        ldr     r3,     =word
+        str     r2,     [r3]
+        
 stop:
 	b	stop
-	
+word:
+        .word   0xdeadbeef
+        
