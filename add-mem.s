3fab0fc347808e9edecdd5b815c96ae5afc4683d
Joby Poriyath <pcjoby@gmail.com>
2013-03-20 19:10:50 +0000

    Copying data from read-only flash to read-write ram

--- add-mem.s
+++ add-mem.s
@@ -6,6 +6,16 @@ result:	.4byte	0
 	.text
 	.global start
 start:
+	ldr	r0,	=flash_sdata
+	ldr	r1,	=ram_sdata
+	ldr	r2,	=data_size
+
+copy:	cmp	r2,	#0
+	ldrneb	r4,	[r0],	#1
+	strneb	r4,	[r1], 	#1
+	sub	r2,	r2,	#1
+	bne	copy
+	
 	ldr	r0,	=val1
 	ldr	r1,	=val2
 	ldr	r2,	[r0]
